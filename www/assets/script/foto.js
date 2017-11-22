bd06dfd4c650178a91fa4d88ba12bd3b569c4256
Noedel-Man <Noedel-Man@hotmail.com>
2017-11-22 15:56:03 +0100

    mc faces en beginnetje met getEmotion

--- www/assets/script/foto.js
+++ www/assets/script/foto.js
@@ -20,6 +20,7 @@ function getMood() {
         alert("taking the picture failed: " + e);
     }, {destinationType: Camera.DestinationType.FILE_URI});
 }
+
 function getlowestMood(mood) {
     // console.info("getlowestMood");
     return "happy";
@@ -35,32 +36,56 @@ function getlowestMood(mood) {
 //        mood.anger
 //        console.info(lowest);
 }
-function sendToServer() {
-//        var ajax =
-    return $.ajax({
-        url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize",
-        beforeSend: function(xhrObj){
+
+function sendToServer(callback) {
+    api = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0";
+    key = "df55abe92f124dd48192b25cc140f18e";
+    callback($.ajax({
+        url: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,emotion",
+        beforeSend: function (xhrObj) {
             // Request headers
-            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Content-Type", "application/json");
 
             // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","4816fe0635a940cfa7b352d347bc9c7c");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", key);
         },
         type: "POST",
         // Request body
-        data: '{"url": "http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg"}',
-        success: function(data) {
-//                console.info(data);
+        data: '{"url": "https://img.buzzfeed.com/buzzfeed-static/static/2014-04/tmp/webdr02/2/17/0e7cd6da3ce720d983515a9ab831a530-3.jpg?downsize=715:*&output-format=auto&output-quality=auto"}',
+        success: function (data) {
+            console.info(data);
             return data;
         },
-        error: function() {
-            console.error('Error occured');
+        error: function () {
+            console.error('Error occured when sending data');
+        }
+    }));
+}
+
+function getEmotion() {
+    var response = sendToServer( function (response) {
+        console.log("yes");
+        var data = response.responseJSON[0].faceAttributes;
+        console.info(data);
+        var emotion = null;
+        for (var key in data.emotion) {
+            console.info(key);
         }
     });
+
+
 }
+
+function getVision() {
+    var api = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0";
+    var key = "2dd18f7e1a9d417faf0c6c1261118e64";
+    sendToServer(api, key);
+}
+
 function setImage(img) {
     document.querySelector(".profile-pic").src = img;
 }
+
 function init() {
     // var lowestMood;
     // sendToServer().done(function (data) {
